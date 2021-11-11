6c7381149f8b933b7bbae50cea5f8e97da906fd4
aymenmelki <79868919+aymenmelki@users.noreply.github.com>
2021-11-11 14:50:17 +0100

    Update EmployeControlTest.java

--- src/test/java/tn/esprit/spring/EmployeControlTest.java
+++ src/test/java/tn/esprit/spring/EmployeControlTest.java
@@ -11,13 +11,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
 import tn.esprit.spring.services.EmployeServiceImpl;
 
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class EmployeControlTest {
 	@Autowired
